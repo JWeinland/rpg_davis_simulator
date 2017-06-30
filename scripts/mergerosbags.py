26fb1451fae7d736bb3635615b456fd0e6fb9ded
jakob@x230 <jakobweinland@gmail.com>
2017-06-30 15:25:59 +0200

    script to merge rosbags

--- scripts/mergerosbags.py
+++ scripts/mergerosbags.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
+
+import rosbag
+
 #header
 __author__ = "Philip Keller, Jakob Weinland"
 __email__ = "philipkellr@gmail.com, jakobweinland@gmail.com"
-import rosbag
 
-
-rbag_dir=("")
-rbag_left_name=("")
-rbag_right_name=("")
+rbag_dir=("/media/jakob/Toshiba 8 G/datasets/rosbags")
+rbag_left_name=("rollingball_2mps_left-20170629-174716.bag")
+rbag_right_name=("rollingball_2mps_right-20170629-175637.bag")
 
 bag1 = rosbag.Bag(rbag_dir + "/" + rbag_left_name,'r')
 bag2 = rosbag.Bag(rbag_dir + "/" + rbag_right_name,'r')
@@ -17,12 +18,11 @@ outputfile = rbag_left_name.split("_")[0] + "_merged"+".bag"
 
 
 def mergebags():
-	with rosbag.Bag(outputfile,"w") as op:
-    	for b in bags:
-        	for topic,msg, t in bag1.read_messages() 
-            	try: 
-                	op.write(topic,msg,t)
+    with rosbag.Bag(outputfile,"w") as op:
+        for b in bags:
+            for topic,msg,t in bag1.read_messages(): 
+                op.write(topic,msg,t)
 
 
-
-
+if (__name__ == '__main__'):
+    mergebags()
